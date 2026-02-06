966fc4f367f71848fbe83e67f4b94d0ac1abf5cb
Milena Lima <milenaserpalima@gmail.com>
2026-02-06 14:17:53 +0100

    Mochawesome installation and configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   e2e: {
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: true,
+      json: true
+    },
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
